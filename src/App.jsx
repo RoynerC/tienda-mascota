835eaafcc0c79486bc995168ce68d2d15d3a9303
KIMBERLYNG <contrerasroyner@gmail.com>
2025-11-27 16:43:15 -0600

    Prueba 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//Importar componente Encabezado.
-import Encabezado from "./components/Encabezado";
+//Importar componente encabezado.
+import Encabezado from "./components/Encabezado.jsx";
 //Importar las vistas.
 import Login from "./views/Login.jsx";
 import Inicio from "./views/Inicio";
